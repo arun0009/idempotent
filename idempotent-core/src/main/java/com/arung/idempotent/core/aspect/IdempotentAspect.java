ae9ff7e1d986cd7d39ef3c497289d03afb8926ea
Arun Gopalpuri <arun@gopalpuri.com>
2024-05-20 17:40:51 -0700

    aspect spel

--- idempotent-core/src/main/java/com/arung/idempotent/core/aspect/IdempotentAspect.java
+++ idempotent-core/src/main/java/com/arung/idempotent/core/aspect/IdempotentAspect.java
@@ -9,7 +9,6 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
@@ -18,7 +17,6 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.time.Instant;
-import java.util.concurrent.TimeUnit;
 
 @Aspect
 public class IdempotentAspect {
@@ -31,15 +29,33 @@ public class IdempotentAspect {
 
     // Create the SpEL parser
     ExpressionParser parser = new SpelExpressionParser();
-    StandardEvaluationContext context = new StandardEvaluationContext();
-
 
     @Around("@annotation(com.arung.idempotent.core.annotation.Idempotent)")
     public Object around(ProceedingJoinPoint pjp) throws Throwable {
-        String key = ((MethodSignature) pjp.getSignature()).getMethod().getAnnotation(Idempotent.class).key();
+        MethodSignature signature = (MethodSignature) pjp.getSignature();
+        Idempotent idempotent = signature.getMethod().getAnnotation(Idempotent.class);
+
+        String key = idempotent.key();
         String processName = processName(pjp);
-        long ttlInSeconds = ((MethodSignature) pjp.getSignature()).getMethod().getAnnotation(Idempotent.class).ttlInSeconds();
+        long ttlInSeconds = idempotent.ttlInSeconds();
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+
+        // Evaluate the SpEL expression if the key is specified
+        if (key != null && !key.isEmpty()) {
+            StandardEvaluationContext context = new StandardEvaluationContext();
+            String[] paramNames = signature.getParameterNames();
+            Object[] args = pjp.getArgs();
+            if (paramNames != null) {
+                for (int i = 0; i < paramNames.length; i++) {
+                    context.setVariable(paramNames[i], args[i]);
+                }
+            } else {
+                throw new IllegalStateException("Parameter names are not available. Ensure the '-parameters' compiler option is enabled.");
+            }
+            key = parser.parseExpression(key).getValue(context, String.class);
+        }
+
+        // If key is still null or empty, get it from the request header
         if ((key == null || key.isEmpty()) && attributes != null) {
             HttpServletRequest request = attributes.getRequest();
             key = request.getHeader("X-Idempotency-Key");
@@ -48,8 +64,13 @@ public class IdempotentAspect {
         IdempotentKey idempotentKey = new IdempotentKey(key, processName);
         Value value = idempotentStore.getValue(idempotentKey);
 
-        if(value != null && value.response() != null && Instant.now().isBefore(Instant.ofEpochSecond(value.expirationTimeInMilliSeconds()))) {
-            return value.response();
+        if(value != null && value.response() != null) {
+            if(value.status().equals(IdempotentStore.Status.COMPLETED.name()))
+                if(Instant.now().isBefore(Instant.ofEpochSecond(value.expirationTimeInMilliSeconds()))) {
+                    return value.response();
+                } else {
+                    idempotentStore.remove(idempotentKey);
+            }
         }
 
         Long expiryTimeInMilliseconds = Instant.now().toEpochMilli() +  (ttlInSeconds * 1000);
@@ -76,6 +97,6 @@ public class IdempotentAspect {
     }
 
     private String processName(ProceedingJoinPoint pjp) {
-        return String.format("%s.%s()", pjp.getTarget().getClass().getSimpleName(), pjp.getSignature().getName());
+        return String.format("__%s.%s()", pjp.getTarget().getClass().getSimpleName(), pjp.getSignature().getName());
     }
 }

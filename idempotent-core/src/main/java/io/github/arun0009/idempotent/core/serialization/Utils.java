7038f43a80ed774ca2b8b44a5500db75beb84f75
burl21 <burl9409@gmail.com>
2026-08-01 18:34:35 +0200

    compute once isResponseEntityPresent

--- idempotent-core/src/main/java/io/github/arun0009/idempotent/core/serialization/Utils.java
+++ idempotent-core/src/main/java/io/github/arun0009/idempotent/core/serialization/Utils.java
@@ -3,10 +3,12 @@ package io.github.arun0009.idempotent.core.serialization;
 import org.springframework.util.ClassUtils;
 
 final class Utils {
+    private static final boolean RESPONSE_ENTITY_PRESENT =
+            ClassUtils.isPresent("org.springframework.http.ResponseEntity", null);
 
     private Utils() {}
 
     static boolean isResponseEntityPresent() {
-        return ClassUtils.isPresent("org.springframework.http.ResponseEntity", null);
+        return RESPONSE_ENTITY_PRESENT;
     }
 }
